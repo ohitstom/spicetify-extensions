5739872721b47c24ed8b56bb3e2414a85e865351
OhItsTom <22730962+ohitstom@users.noreply.github.com>
2023-09-29 01:36:21 +0100

    refactor(pi): rewrite of dumb logic

--- pixelatedImages/pixelatedImages.js
+++ pixelatedImages/pixelatedImages.js
@@ -3,37 +3,26 @@
 // DESCRIPTION: Convert images into a pseudo pixel art.
 
 (function pixelatedImages() {
-	// Element creation + Attributes
-	const svgElement = document.createElementNS("http://www.w3.org/2000/svg", "svg");
-	svgElement.setAttribute("style", "height: 0;");
-	const filterElement = document.createElementNS("http://www.w3.org/2000/svg", "filter");
-	filterElement.setAttribute("id", "pixelate");
-	filterElement.setAttribute("x", "0");
-	filterElement.setAttribute("y", "0");
-	const feFloodElement = document.createElementNS("http://www.w3.org/2000/svg", "feFlood");
-	feFloodElement.setAttribute("x", "0");
-	feFloodElement.setAttribute("y", "0");
-	feFloodElement.setAttribute("height", "2");
-	feFloodElement.setAttribute("width", "2");
-	const feCompositeElement = document.createElementNS("http://www.w3.org/2000/svg", "feComposite");
-	feCompositeElement.setAttribute("width", "10");
-	feCompositeElement.setAttribute("height", "10");
-	const feTileElement = document.createElementNS("http://www.w3.org/2000/svg", "feTile");
-	feTileElement.setAttribute("result", "a");
-	const feCompositeElement2 = document.createElementNS("http://www.w3.org/2000/svg", "feComposite");
-	feCompositeElement2.setAttribute("in", "SourceGraphic");
-	feCompositeElement2.setAttribute("in2", "a");
-	feCompositeElement2.setAttribute("operator", "in");
-	const feMorphologyElement = document.createElementNS("http://www.w3.org/2000/svg", "feMorphology");
-	feMorphologyElement.setAttribute("operator", "dilate");
-	feMorphologyElement.setAttribute("radius", "5");
+	const svgNS = "http://www.w3.org/2000/svg";
 
-	// Append to body
-	filterElement.append(feFloodElement, feCompositeElement, feTileElement, feCompositeElement2, feMorphologyElement);
+	// Create SVG filter elements
+	const filterElement = document.createElementNS(svgNS, "filter");
+	filterElement.setAttribute("id", "pixelate");
+	filterElement.innerHTML = `
+      <feFlood x="0" y="0" height="2" width="2" />
+      <feComposite width="10" height="10" />
+      <feTile result="a" />
+      <feComposite in="SourceGraphic" in2="a" operator="in" />
+      <feMorphology operator="dilate" radius="5" />
+  `;
+
+	// Append SVG filter to body
+	const svgElement = document.createElementNS(svgNS, "svg");
+	svgElement.setAttribute("style", "height: 0;");
 	svgElement.appendChild(filterElement);
 	document.body.appendChild(svgElement);
 
-	// Stylesheet
+	// Apply CSS style
 	const cssStyleElement = document.createElement("style");
 	cssStyleElement.textContent = `img { filter: url('#pixelate'); }`;
 	document.head.appendChild(cssStyleElement);
