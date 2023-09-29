14be3673a7ab60f74e373ab6a4360a524d3f99fc
OhItsTom <22730962+ohitstom@users.noreply.github.com>
2023-09-29 01:16:33 +0100

    refactor(vp): improve tippy handling

--- volumePercentage/volumePercentage.js
+++ volumePercentage/volumePercentage.js
@@ -3,23 +3,39 @@
 // DESCRIPTION: Display volume percentage but in a Tippy!
 
 (function volumePercentage() {
-    const volumeBar = document.querySelector(".main-nowPlayingBar-volumeBar .progress-bar")
-    if (!(volumeBar && Spicetify.Player && Spicetify.Tippy && Spicetify.TippyProps)) {
+    const volumeBar = document.querySelector(".main-nowPlayingBar-volumeBar .progress-bar");
+    const volumeSlider = document.querySelector(".main-nowPlayingBar-volumeBar .progress-bar__slider");
+    
+    if (!(volumeBar && volumeSlider && Spicetify.Platform.PlaybackAPI && Spicetify.Tippy && Spicetify.TippyProps)) {
         setTimeout(volumePercentage, 200);
         return;
-    }
+    };
     
     const tippyContainer = Spicetify.Tippy(volumeBar, {
         ...Spicetify.TippyProps,
-        content: `%`,
         hideOnClick: false
     });
 
-    updatePercentage();
-    function updatePercentage() {
-        const currVolume = Math.round((Spicetify.Player?.origin?._volume?._volume ?? Spicetify.Platform?.PlaybackAPI?._volume) * 100);
+    const updatePercentage = () => {
+        const currVolume = Math.round(Spicetify.Platform.PlaybackAPI._volume * 100);
         tippyContainer.setContent(currVolume == -100 ? `` : `${currVolume}%`);
-    }
-    if (Spicetify.Platform?.PlaybackAPI === undefined) Spicetify.Player.origin._events.addListener("volume", updatePercentage);
-    else Spicetify.Platform.PlaybackAPI._events.addListener("volume", updatePercentage);
+    };
+    
+    // Event listeners
+    volumeSlider.addEventListener('mousedown', (event) => {
+        tippyContainer.setProps({trigger: "mousedown"});
+        
+      const onMouseUp = (event) => {
+        tippyContainer.setProps({trigger: "mouseenter focus"});
+        if (event.srcElement !== volumeSlider) tippyContainer.hide();
+        document.removeEventListener('mouseup', onMouseUp);
+      };
+    
+      document.addEventListener('mouseup', onMouseUp);
+    }, { capture: true });
+    
+    Spicetify.Platform.PlaybackAPI._events.addListener("volume", updatePercentage);
+
+    // Initialize
+    updatePercentage();
 })();
