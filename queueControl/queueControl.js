9548e3bc1a48802c650171c5911c5525a66289e3
OhItsTom <22730962+ohitstom@users.noreply.github.com>
2023-11-09 21:16:28 +0000

    update icons

--- queueControl/queueControl.js
+++ queueControl/queueControl.js
@@ -14,7 +14,7 @@
 			Spicetify.TippyProps
 		)
 	) {
-		setTimeout(queueControl, 200);
+		setTimeout(queueControl, 10);
 		return;
 	}
 
@@ -22,9 +22,9 @@
 		const [isQueued, setIsQueued] = Spicetify.React.useState(Spicetify.Platform.PlayerAPI.getQueue().queued.some(item => item.uri === uri));
 
 		Spicetify.React.useEffect(() => {
-			//Spicetify.Platform.PlayerAPI._queue._events.addListener("queue_update", e => console.log(uri, "signal receive"));
+			//Spicetify.Platform.PlayerAPI._queue._events.addListener("queue_update", e => console.debug(uri, "signal receive"));
 			return function cleanup() {
-				console.log("exit");
+				console.debug("exit");
 			};
 		}, [uri, tippy]);
 
@@ -46,7 +46,8 @@
 				className:
 					"Button-sm-16-buttonTertiary-iconOnly-isUsingKeyboard-useBrowserDefaultFocusStyle Button-textSubdued-small-small-buttonTertiary-iconOnly-condensed-isUsingKeyboard-useBrowserDefaultFocusStyle main-trackList-rowHeartButton",
 				"aria-checked": false,
-				onClick: handleClick
+				onClick: handleClick,
+				style: { marginRight: "8px" }
 			},
 			Spicetify.React.createElement(
 				"span",
@@ -61,7 +62,11 @@
 						className: "Svg-img-icon-small"
 					},
 					Spicetify.React.createElement("svg", {
-						dangerouslySetInnerHTML: { __html: isQueued ? Spicetify.SVGIcons.block : Spicetify.SVGIcons.queue }
+						dangerouslySetInnerHTML: {
+							__html: isQueued
+								? `<path d="M5.25 3v-.917C5.25.933 6.183 0 7.333 0h1.334c1.15 0 2.083.933 2.083 2.083V3h4.75v1.5h-.972l-1.257 9.544A2.25 2.25 0 0 1 11.041 16H4.96a2.25 2.25 0 0 1-2.23-1.956L1.472 4.5H.5V3h4.75zm1.5-.917V3h2.5v-.917a.583.583 0 0 0-.583-.583H7.333a.583.583 0 0 0-.583.583zM2.986 4.5l1.23 9.348a.75.75 0 0 0 .744.652h6.08a.75.75 0 0 0 .744-.652L13.015 4.5H2.985z"></path>`
+								: `<path d="M16 15H2v-1.5h14V15zm0-4.5H2V9h14v1.5zm-8.034-6A5.484 5.484 0 0 1 7.187 6H13.5a2.5 2.5 0 0 0 0-5H7.966c.159.474.255.978.278 1.5H13.5a1 1 0 1 1 0 2H7.966zM2 2V0h1.5v2h2v1.5h-2v2H2v-2H0V2h2z"></path>`
+						}
 					})
 				)
 			)
