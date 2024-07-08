cb4a97aaf49374ca39c8dcc2f6aea9e2734b3ca5
OhItsTom <22730962+ohitstom@users.noreply.github.com>
2024-07-08 11:11:26 +0100

    refactor: optimize ConfirmDialog component in spotifyBackup.js

--- spotifyBackup/spotifyBackup.js
+++ spotifyBackup/spotifyBackup.js
@@ -34,20 +34,30 @@
 	const Dialog = Spicetify.React.memo(props => {
 		const [state, setState] = Spicetify.React.useState(true);
 		const self = document.querySelector(".ReactModalPortal:last-of-type");
-
-		return Spicetify.ReactComponent.ConfirmDialog({
+		const ConfirmDialog = Spicetify.ReactComponent.ConfirmDialog;
+		const isForwardRef = typeof ConfirmDialog === "function";
+		const commonProps = {
 			...props,
 			isOpen: state,
 			onClose: () => {
 				setState(false);
+				props.onClose?.();
 				self.remove();
 			},
 			onConfirm: () => {
 				setState(false);
-				props.onConfirm();
+				props.onConfirm?.();
 				self.remove();
 			}
-		});
+		};
+
+		Spicetify.React.useEffect(() => {
+			if (state) {
+				props.onOpen?.();
+			}
+		}, [state]);
+
+		return isForwardRef ? ConfirmDialog(commonProps) : Spicetify.React.createElement(ConfirmDialog, commonProps);
 	});
 
 	// Create our own row matching Spotify's
