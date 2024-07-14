b6d8255292de9bbaa224b7f2f2a49d47d10f3100
Sanooj Es <sanoojes6371@gmail.com>
2024-07-14 18:11:13 +0530

    feat(npvAmbience): improved transition, add border bottom
    this commit changes the transition, background color, add adds border-bottom to the header when active

--- npvAmbience/npvAmbience.js
+++ npvAmbience/npvAmbience.js
@@ -37,11 +37,12 @@
 			width: 100%;
 			z-index: 1;
 			background: transparent;
+			transition: background-color 0.25s, backdrop-filter 0.5s, opacity 0.4s ease-out;
 		}
 		aside[aria-label="Now playing view"] .fAte2d0xETy7pnDUAgHY, aside[aria-label="Now playing view"] .mdMUqcSHFw1lZIcYEblu {
-			background-color: rgb(var(--spice-rgb-main), 0.75) !important;
-    	backdrop-filter: blur(1rem);
-			transition: background-color 0.25s, backdrop-filter 0.25s, opacity 0.4s ease-out;
+			background-color: rgba(var(--spice-rgb-main), 0.2) !important;
+			backdrop-filter: blur(24px) saturate(140%);
+			border-bottom: 1px solid rgba(var(--spice-rgb-selected-row),0.2);
 		}
 
 		aside[aria-label="Now playing view"]:has(.ZbDMGdU4aBOnrNLowNRq) .main-buddyFeed-scrollBarContainer:not(:has(.main-buddyFeed-content > .main-buddyFeed-header)), aside[aria-label="Now playing view"]:has(.W3E0IT3_STcazjTeyOJa) .cZCuJDjrGA2QMXja_Sua:not(:has(.AAdBM1nhG73supMfnYX7 > .fNXmHtlrj4UVWmhQrJ_5)) {
