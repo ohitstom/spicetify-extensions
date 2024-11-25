2c44187e31af1e0659b5cf66cba0ce2f5ce76246
OhItsTom <22730962+ohitstom@users.noreply.github.com>
2024-11-25 16:51:45 +0000

    resolve #22 #26

--- noControls/noControls.js
+++ noControls/noControls.js
@@ -7,7 +7,16 @@
 		setTimeout(noControls, 10);
 		return;
 	}
+	if (Spicetify.Platform.UpdateAPI._updateUiClient.updateTitlebarHeight) {
+		Spicetify.Platform.UpdateAPI._updateUiClient.updateTitlebarHeight({
+			"height": 1
+		  });
+	}
 
+	if (Spicetify.Platform.UpdateAPI._updateUiClient.setButtonsVisibility) {
+		Spicetify.Platform.UpdateAPI._updateUiClient.setButtonsVisibility(false);
+	}
+	
 	await Spicetify.CosmosAsync.post("sp://messages/v1/container/control", {
 		type: "update_titlebar",
 		height: "1px"
